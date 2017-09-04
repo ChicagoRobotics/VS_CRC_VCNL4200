bc2cb4e60993d0852d016ce1ca0ebdb0fd215159
Jamie Laing <jamie@jamielaing.com>
2017-09-03 21:34:45 -0500

    initial setup

--- VS_CRC_VCNL4200/VS_CRC_VCNL4200.ino
+++ VS_CRC_VCNL4200/VS_CRC_VCNL4200.ino
@@ -4,12 +4,27 @@
  Author:	jlaing
 */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+#include "CRC_VCNL4200.h"
+CRC_VCNL4200 vcnl4200;
 
+
+void setup() {
+	Serial.begin(9600);
+	while (!Serial);
+	Serial.println("Booting.");
+	if (vcnl4200.exists()) {
+		Serial.println("VCNL4200 found");
+		vcnl4200.initialize();
+		Serial.println("VCNL4200 initialized");
+	}
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	Serial.print("Proximity: ");
+	Serial.println(vcnl4200.getProximity());
+	Serial.print("Ambient: ");
+	Serial.println(vcnl4200.getAmbient());
+	Serial.println();
+	delay(2000);
 }
