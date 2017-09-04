ae806c7fb7b8f3a55a22d96227c42fe8e6aed3a6
Jamie Laing <jamie@jamielaing.com>
2017-09-03 21:20:24 -0500

    Add project files.

---
+++ VS_CRC_VCNL4200/VS_CRC_VCNL4200.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		VS_CRC_VCNL4200.ino
+ Created:	9/3/2017 9:20:20 PM
+ Author:	jlaing
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
